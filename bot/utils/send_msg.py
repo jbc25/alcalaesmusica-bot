28232f103504b4f9448420a0627fc5430b436f23
julio <jberzal86@gmail.com>
2023-05-12 12:22:53 +0200

    - fixed bug Timeout with Telegram api in long running task - Added Threads - fixed bug with sending photo to all (notifying festivals)

--- bot/utils/send_msg.py
+++ bot/utils/send_msg.py
@@ -12,23 +12,6 @@ def send_to_all_thread(bot, message):
     else:
         user_chats = UserChat.objects.filter(is_active=True)
 
-    time.sleep(10)
-    for i in range(50):
-        time.sleep(2)
-        chat_id = user_chats.first().id_chat
-        bot.send_message(chat_id=chat_id, text=f'{message} - {i}', parse_mode="HTML",
-                     disable_web_page_preview=True, reply_markup=telegram.ReplyKeyboardRemove())
-
-
-def send_to_all(bot, message):
-    # send_to_all_thread(bot, message)
-    thread = Thread(target = send_to_all_thread, args = (bot, message))
-    thread.start()
-
-    time.sleep(5)
-
-    return
-
     for user_chat in user_chats:
         time.sleep(0.3)
         chat_id = user_chat.id_chat
@@ -36,14 +19,19 @@ def send_to_all(bot, message):
         try:
             bot.send_message(chat_id=chat_id, text=message, parse_mode="HTML",
                          disable_web_page_preview=True, reply_markup=telegram.ReplyKeyboardRemove())
-            user_chat.is_active = True
         except telegram.error.Unauthorized:
             user_chat.is_active = False
 
         user_chat.save()
 
+def send_to_all(bot, message):
+    thread = Thread(target=send_to_all_thread, args = (bot, message))
+    thread.start()
 
-def send_photo_to_all(bot, photo, caption, reply_markup, initial_text=None):
+
+
+def send_photo_to_all_thread(bot, photo, caption, reply_markup, initial_text=None):
+
     if developing:
         user_chats = UserChat.objects.filter(id_chat=dev_chat_id)
     else:
@@ -52,16 +40,19 @@ def send_photo_to_all(bot, photo, caption, reply_markup, initial_text=None):
     for user_chat in user_chats:
         time.sleep(0.3)
         chat_id = user_chat.id_chat
-        if initial_text:
-            bot.send_message(chat_id=chat_id, text=initial_text, parse_mode="HTML", disable_web_page_preview=True)
 
         try:
+            if initial_text:
+                bot.send_message(chat_id=chat_id, text=initial_text, parse_mode="HTML", disable_web_page_preview=True)
             bot.send_photo(chat_id=chat_id, photo=photo, caption=caption, parse_mode="HTML", reply_markup=reply_markup)
-            user_chat.is_active = True
         except telegram.error.Unauthorized:
             user_chat.is_active = False
 
         user_chat.save()
+
+def send_photo_to_all(bot, photo, caption, reply_markup, initial_text=None):
+    thread = Thread(target=send_photo_to_all_thread, args = (bot, photo, caption, reply_markup, initial_text))
+    thread.start()
 
 
 def send_dev_chat_message(bot, message):
